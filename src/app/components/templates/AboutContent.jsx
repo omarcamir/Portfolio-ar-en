88936ed0c861bed5bf6d25c721f2d86413c4728a
omarcamir <omarcamirr@gmail.com>
2024-11-24 19:14:47 +0200

    fix about

--- src/app/components/templates/AboutContent.jsx
+++ src/app/components/templates/AboutContent.jsx
@@ -1,10 +1,14 @@
 import PageHeader from "../organisms/PageHeader";
 import person from '../../../assets/images/person2.webp'
+import Statistics from "../organisms/Statistics";
+import MySkills from "../organisms/MySkills";
 
 const AboutContent = () => {
   return (
     <div className="container mx-auto py-10">
       <PageHeader page={"About"} imgSrc={person} />
+      <Statistics/>
+      <MySkills/>
     </div>
   );
 };
